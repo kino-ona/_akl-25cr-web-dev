1b743e6be676b793f09765c6335980759fcdd9f9
Donguk Jung <wjdehd7991@gmail.com>
2022-08-24 17:49:32 +0900

    sendCaptureImage 변경

--- src/functions/sendCaptureImage.js
+++ src/functions/sendCaptureImage.js
@@ -1,8 +1,10 @@
 export default function sendCaptureImage() {
+    console.log("SendCapture Image :::::::: ", window.captureData);
     if(window.isMobile.any()) {
         if(window.isMobile.Android()) {
             AndroidBridge.sendCaptureImage(window.captureData);
         } else if(window.isMobile.iOS()) {
+            return window.captureData
         }
     } else {
         console.log("SendCapture Image :::::::: ", window.captureData);
