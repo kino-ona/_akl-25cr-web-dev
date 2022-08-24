1b13ded719cd8b64d16467fceb5bd4fe9e137786
Donguk Jung <wjdehd7991@gmail.com>
2022-08-24 17:07:27 +0900

    sendCaptureImage 롤백

--- src/functions/sendCaptureImage.js
+++ src/functions/sendCaptureImage.js
@@ -1,16 +1,12 @@
-// 공유 관련 버튼 클릭 시 호출해야 하는 함수
-// import sendMessage from './sendMessage';
 export default function sendCaptureImage() {
-    const captureData = sessionStorage.getItem("captureData");
     if(window.isMobile.any()) {
         if(window.isMobile.Android()) {
-            AndroidBridge.sendCaptureImage(captureData);
+            AndroidBridge.sendCaptureImage(window.captureData);
         } else if(window.isMobile.iOS()) {
-            return captureData;
+            return window.captureData;
         }
     } else {
-        console.log("SendCapture Image :::::::: ", captureData);
-        return captureData;
+        console.log("SendCapture Image :::::::: ", window.captureData);
+        return window.captureData;
     }
 }
-
--- src/functions/windowCapture.js
+++ src/functions/windowCapture.js
@@ -1,6 +1,6 @@
 import html2canvas from 'html2canvas';
 export default function windowCapture() {
     html2canvas(document.getElementById("app")).then(canvas => {
-        sessionStorage.setItem("captureData", canvas.toDataURL());
+        window.captureData = canvas.toDataURL();
     });
 }
