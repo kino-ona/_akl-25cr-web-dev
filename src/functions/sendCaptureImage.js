354e6e461305bac0c4ee4a4220b8665968bf9a47
Donguk Jung <wjdehd7991@gmail.com>
2022-08-24 17:16:02 +0900

    sendCaptureImage 롤백

--- src/functions/sendCaptureImage.js
+++ src/functions/sendCaptureImage.js
@@ -1,12 +1,12 @@
 export default function sendCaptureImage() {
-    if(window.isMobile.any()) {
-        if(window.isMobile.Android()) {
-            AndroidBridge.sendCaptureImage(window.captureData);
-        } else if(window.isMobile.iOS()) {
-            return window.captureData;
-        }
-    } else {
-        console.log("SendCapture Image :::::::: ", window.captureData);
-        return window.captureData;
-    }
+    return window.captureData;
+    // if(window.isMobile.any()) {
+    //     if(window.isMobile.Android()) {
+    //         AndroidBridge.sendCaptureImage(window.captureData);
+    //     } else if(window.isMobile.iOS()) {
+    //     }
+    // } else {
+    //     console.log("SendCapture Image :::::::: ", window.captureData);
+    //     return window.captureData;
+    // }
 }
