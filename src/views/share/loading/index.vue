e8ca5fca7db23d50ababfa9d4e52e2a629f6ebb5
Donguk Jung <wjdehd7991@gmail.com>
2022-08-26 18:26:03 +0900

    로딩페이지 에러 개선

--- src/views/share/loading/index.vue
+++ src/views/share/loading/index.vue
@@ -21,15 +21,15 @@ export default {
         console.log("Mobile로 접근 성공")
         let url = {'func' : 'setData','callback' : 'setData','param' : 'null'};
         window.getData(url);
-        setTimeout(function () {
-          this.$router.push({
-            path: result.path
-          })
-        }, 1000);
+        // setTimeout(function () {
+        this.$router.push({
+          path: result.path
+        })
+        // }, 1000);
       // Web상에서 테스트 하는 경우
       } else {
         console.log("Web 상에서 Test 진행 :::::::::::: ", this.result);
-        let mainData = '%7B%0A%20%20%22date%22%3A%20%222022%EB%85%84%201%EC%9B%94%2013%EC%9D%BC%20%EB%AA%A9%EC%9A%94%EC%9D%BC%22%2C%0A%20%20%22term%22%3A%20%22D%22%2C%0A%20%20%22exeType%22%3A%20%22M%22%2C%0A%20%20%22dataList%22%3A%20%5B%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%207%EC%9D%BC%22%2C%20%22data%22%3A%20200%2C%20%22isSelected%22%3A%20false%7D%2C%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%208%EC%9D%BC%22%2C%20%22data%22%3A%20233%2C%20%22isSelected%22%3A%20false%7D%2C%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%209%EC%9D%BC%22%2C%20%22data%22%3A%20800%2C%20%22isSelected%22%3A%20false%7D%2C%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%2010%EC%9D%BC%22%2C%20%22data%22%3A%201033%2C%20%22isSelected%22%3A%20false%7D%2C%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%2011%EC%9D%BC%22%2C%20%22data%22%3A%202010%2C%20%22isSelected%22%3A%20true%7D%2C%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%2012%EC%9D%BC%22%2C%20%22data%22%3A%201500%2C%20%22isSelected%22%3A%20false%7D%2C%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%2013%EC%9D%BC%22%2C%20%22data%22%3A%20900%2C%20%22isSelected%22%3A%20false%7D%2C%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%2014%EC%9D%BC%22%2C%20%22data%22%3A%20750%2C%20%22isSelected%22%3A%20false%7D%2C%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%2015%EC%9D%BC%22%2C%20%22data%22%3A%202033%2C%20%22isSelected%22%3A%20false%7D%0A%20%20%5D%2C%0A%20%20%22avgData%22%3A%201000%2C%0A%20%20%22maxData%22%3A%202022%0A%7D'
+        let mainData = '%7B%0A%20%20%22date%22%3A%20%222022%EB%85%84%201%EC%9B%94%2013%EC%9D%BC%20%EB%AA%A9%EC%9A%94%EC%9D%BC%22%2C%0A%20%20%22totMusclePoint%22%3A%200%2C%0A%20%20%22maxMusclePoint%22%3A%200%2C%0A%20%20%22maxRpm%22%3A%200%2C%0A%20%20%22avgRpm%22%3A%200%2C%0A%20%20%22totCalories%22%3A%200%2C%0A%20%20%22totExerciseTime%22%3A%200%2C%0A%20%20%22totDistance%22%3A%200%2C%0A%20%20%22recentlyVod%22%3A%20%7B%0A%20%20%20%20%22classId%22%3A%20%220001%22%2C%0A%20%20%20%20%22lectureName%22%3A%20%22%EC%8A%A4%ED%94%BC%EB%8B%9D%EA%B0%95%EC%82%AC%22%2C%0A%20%20%20%20%22classNm%22%3A%20%22%EC%8B%A0%EB%82%98%EB%8A%94%EC%8A%A4%ED%94%BC%EB%8B%9D%EC%9D%98%22%2C%0A%20%20%20%20%22calories%22%3A%20100%0A%20%20%7D%2C%0A%20%20%22recentlyLive%22%3A%20%7B%0A%20%20%20%20%22classId%22%3A%20%220002%22%2C%0A%20%20%20%20%22lectureName%22%3A%20%22%EC%8A%A4%ED%94%BC%EB%8B%9D%EB%AA%BD%ED%82%A4%EC%8C%A4%22%2C%0A%20%20%20%20%22classNm%22%3A%20%22%ED%99%94%EB%A0%A4%ED%95%9C%EC%A1%B0%EB%AA%85%EC%9D%B4%EB%82%98%EB%A5%BC%EA%B0%90%EC%8B%B8%EB%84%A4%22%2C%0A%20%20%20%20%22calories%22%3A%20200%0A%20%20%7D%2C%0A%20%20%22shareType%22%3A%201%0A%7D'
         window.setData(mainData);
         this.$router.push({
           path: result.path
