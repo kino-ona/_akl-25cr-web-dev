2533068a68d9bd4a0abf2c038238fbdc7d7b92b4
Donguk Jung <wjdehd7991@gmail.com>
2022-08-29 13:56:19 +0900

    windowCapture 시 1초간 딜레이 후 캡쳐하도록 변경

--- src/functions/windowCapture.js
+++ src/functions/windowCapture.js
@@ -1,6 +1,9 @@
 import html2canvas from 'html2canvas';
 export default function windowCapture() {
-    html2canvas(document.getElementById("app")).then(canvas => {
-        window.captureData = canvas.toDataURL();
-    });
+    setTimeout(function(){
+        html2canvas(document.getElementById("app")).then(canvas => {
+            window.captureData = canvas.toDataURL();
+            console.log("window.captureData >>>>>>>>>>>>", window.captureData)
+        });
+    }, 1000)
 }
