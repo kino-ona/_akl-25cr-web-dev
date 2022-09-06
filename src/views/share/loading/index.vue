d442eed835e68ba00fbf5a8b9278207d613f1daa
Donguk Jung <wjdehd7991@gmail.com>
2022-09-06 12:53:55 +0900

    Syntax Error를 잡기 위해 Router 작동 방식 변경

--- src/views/share/loading/index.vue
+++ src/views/share/loading/index.vue
@@ -31,6 +31,9 @@ export default {
         let mainData = '%7B%0A%20%20%22date%22%3A%20%222022%EB%85%84%201%EC%9B%94%2013%EC%9D%BC%20%EB%AA%A9%EC%9A%94%EC%9D%BC%22%2C%0A%20%20%22term%22%3A%20%22W%22%2C%0A%20%20%22exeType%22%3A%20%22K%22%2C%0A%20%20%22dataList%22%3A%20%5B%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%207%EC%9D%BC%22%2C%20%22data%22%3A%20200%2C%20%22isSelected%22%3A%20false%7D%2C%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%208%EC%9D%BC%22%2C%20%22data%22%3A%20233%2C%20%22isSelected%22%3A%20false%7D%2C%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%209%EC%9D%BC%22%2C%20%22data%22%3A%20800%2C%20%22isSelected%22%3A%20false%7D%2C%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%2010%EC%9D%BC%22%2C%20%22data%22%3A%201033%2C%20%22isSelected%22%3A%20false%7D%2C%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%2011%EC%9D%BC%22%2C%20%22data%22%3A%202010%2C%20%22isSelected%22%3A%20true%7D%2C%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%2012%EC%9D%BC%22%2C%20%22data%22%3A%201500%2C%20%22isSelected%22%3A%20false%7D%2C%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%2013%EC%9D%BC%22%2C%20%22data%22%3A%20900%2C%20%22isSelected%22%3A%20false%7D%2C%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%2014%EC%9D%BC%22%2C%20%22data%22%3A%20750%2C%20%22isSelected%22%3A%20false%7D%2C%0A%20%20%20%20%7B%22date%22%3A%221%EC%9B%94%2015%EC%9D%BC%22%2C%20%22data%22%3A%202033%2C%20%22isSelected%22%3A%20false%7D%0A%20%20%5D%2C%0A%20%20%22avgData%22%3A%201000%2C%0A%20%20%22maxData%22%3A%202022%0A%7D'
         window.setData(mainData);
       }
+
+      console.log("이동할 경로 정보입니다. ::::::::::: ", result.path)
+
       this.$router.push({
         path: result.path
       }).catch((reason)=>{
