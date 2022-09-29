705ea2b6b39ce25bd1e6f8dcaf73d1b259764387
Donguk Jung <wjdehd7991@gmail.com>
2022-09-29 13:59:26 +0900

    캡처한 이미지 Crop하는 기능 추가

--- src/functions/windowCapture.js
+++ src/functions/windowCapture.js
@@ -2,8 +2,15 @@ import html2canvas from 'html2canvas';
 export default function windowCapture() {
     window.setTimeout(function(){
         html2canvas(document.getElementById("app")).then(canvas => {
-            window.captureData = canvas.toDataURL();
-            console.log("window.captureData >>>>>>>>>>>>", window.captureData)
+            var extra_canvas = document.createElement("canvas");
+            extra_canvas.setAttribute('width',canvas.width);
+            extra_canvas.setAttribute('height',canvas.height);
+
+            var ctx = extra_canvas.getContext('2d');
+            ctx.drawImage(canvas,0,0,canvas.width, canvas.height,0,0,canvas.width - 5,canvas.height - 5);
+            window.captureData =  extra_canvas.toDataURL();
+            console.log("######### window.captureData #########")
+            console.log(window.captureData)
         });
     }, 1000)
 }
