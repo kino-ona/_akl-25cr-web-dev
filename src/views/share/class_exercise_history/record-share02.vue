219c6ec712939b496c458705a56e4c701f5664cd
minji <gdfd738@gmail.com>
2023-04-25 13:28:04 +0900

    ACT-1290, 운동공유하기,header__text 영역 width(250) 늘리기

--- src/views/share/class_exercise_history/record-share02.vue
+++ src/views/share/class_exercise_history/record-share02.vue
@@ -155,7 +155,7 @@ export default {
       line-height: 18px;
       margin-bottom: 4px;
       color: $gray02;
-      width: 220px;
+      width: 250px;
       overflow: hidden;
       text-overflow: ellipsis;
       white-space: nowrap;
