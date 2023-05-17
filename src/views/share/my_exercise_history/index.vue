0b6d52b65b805d88d8fd551319c435617b09a3ef
dlrpfl <dlrpfl@ona.co.kr>
2023-05-17 17:36:02 +0900

    Update index.vue

--- src/views/share/my_exercise_history/index.vue
+++ src/views/share/my_exercise_history/index.vue
@@ -2,7 +2,7 @@
   <section class="exercise-share">
     <div class="container">
       <header class="exercise-share__header">
-        <h2 class="header__title">{{ this.result.date }}</h2>
+        <h2 class="header__title">2022년 1월 13일 목요일{{ this.result.date }}</h2>
         <img class="logo" src="@/assets/logo.png" alt="로고" />
       </header>
       <div class="exercise-share__body">
@@ -12,7 +12,7 @@
             <span>총 머슬 포인트</span>
           </h1>
           <div class="exercise-share__point">
-            <span class="point__value">{{ this.result.totMusclePoint }}</span>
+            <span class="point__value">10,800{{ this.result.totMusclePoint }}</span>
             <span class="point__unit">점</span>
           </div>
 
@@ -21,7 +21,7 @@
               <div class="record__panel">
                 <h2 class="panel__title">최고 머슬 포인트</h2>
                 <div class="panel__point text-primary">
-                  <span class="point__value">{{ this.result.maxMusclePoint }}</span>
+                  <span class="point__value">2,600.5{{ this.result.maxMusclePoint }}</span>
                   <span class="point__unit">점</span>
                 </div>
               </div>
@@ -30,14 +30,14 @@
               <div class="record__panel">
                 <h2 class="panel__title">최고 케이던스</h2>
                 <div class="panel__point text-blue">
-                  <span class="point__value">{{ Math.round(this.result.maxRpm) }}</span>
+                  <span class="point__value">120<!-- {{ Math.round(this.result.maxRpm) }} --></span>
                   <span class="point__unit">rpm</span>
                 </div>
               </div>
               <div class="record__panel">
                 <h2 class="panel__title">평균 케이던스</h2>
                 <div class="panel__point text-blue">
-                  <span class="point__value">{{ Math.round(this.result.avgRpm) }}</span>
+                  <span class="point__value">120<!-- {{ Math.round(this.result.avgRpm) }} --></span>
                   <span class="point__unit">rpm</span>
                 </div>
               </div>
@@ -49,12 +49,12 @@
               <div class="record-share__item">
                 <div class="d-flex align-items-center">
                   <img src="@/assets/icons/icon-fire.svg" class="w-34 mr-10" alt="소모칼로리 아이콘"/>
-                  <span class="text-16 font-weight-800">소모 칼로리</span>
+                  <span class="record-share__text">소모 칼로리</span>
                 </div>
 
-                <div class="d-flex align-items-end line-height-1">
-                  <span class="text-20 font-weight-800">{{ this.result.totCalories }}</span>
-                  <span class="text-14 font-weight-800 ml-4">kcal</span>
+                <div class="record-share__right">
+                  <span class="record-share__data">9,9999{{ this.result.totCalories }}</span>
+                  <span class="record-share__text">kcal</span>
                 </div>
               </div>
             </li>
@@ -62,12 +62,12 @@
               <div class="record-share__item">
                 <div class="d-flex align-items-center">
                   <img src="@/assets/icons/icon-exercise-distance.svg" class="w-34 mr-10" alt="운동 거리 아이콘" />
-                  <span class="text-16 font-weight-800">운동 거리</span>
+                  <span class="record-share__text">운동 거리</span>
                 </div>
 
-                <div class="d-flex align-items-end line-height-1">
-                  <span class="text-20 font-weight-800">{{ this.result.totDistance }}</span>
-                  <span class="text-14 font-weight-800 ml-4">km/h</span>
+                <div class="record-share__right">
+                  <span class="record-share__data">99999.99{{ this.result.totDistance }}</span>
+                  <span class="record-share__text">km/h</span>
                 </div>
               </div>
             </li>
@@ -75,11 +75,11 @@
               <div class="record-share__item">
                 <div class="d-flex align-items-center">
                   <img src="@/assets/icons/icon-exercise-time.svg" class="w-34 mr-10" alt="운동 시간 아이콘" />
-                  <span class="text-16 font-weight-800">운동 시간</span>
+                  <span class="record-share__text">운동 시간</span>
                 </div>
 
-                <div class="d-flex align-items-end line-height-1">
-                  <span class="text-20 font-weight-800">{{ getTime(this.result.totExerciseTime) }}</span>
+                <div class="record-share__right">
+                  <span class="record-share__data">{{ getTime(this.result.totExerciseTime) }}</span>
                 </div>
               </div>
             </li>
@@ -90,9 +90,9 @@
             합산 운동 이력입니다.
           </p>
         </section>
-        <section class="exercise-share__section" v-if="(this.result.shareType != 2)">
+        <!-- <section class="exercise-share__section" v-if="(this.result.shareType != 2)">
           <h2 class="section__title">클럽타올라 기록</h2>
-          <ul  v-if="this.isInClubTaolaData" class="exercise-share__record-box list-style-none">
+          <ul v-if="this.isInClubTaolaData" class="exercise-share__record-box list-style-none">
             <li v-for="(classItem, index) in this.result['recentlyClassList']" :key="index">
               <div class="record__item">
                 <div class="record__detail">
@@ -112,8 +112,43 @@
           <div v-else class="exercise-share__empty-data">
             클럽타올라 기록이 없습니다.
           </div>
+        </section> -->
+        <section class="exercise-share__section">
+          <h2 class="section__title">클럽타올라 기록</h2>
+          <ul class="exercise-share__record-box list-style-none">
+            <li>
+              <div class="record__item">
+                <div class="record__detail">
+                  <img src="@/assets/icons/icon-vod.svg" class="w-34" />
+                  <div class="line-height-1">
+                    <h3 class="detail__title">22’ 5월 3일 SPINNING LIVE CLASS ㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㅁㄹ</h3>
+                    <span class="detail__text">디파이, 암웨이 강사님 ㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹ</span>
+                  </div>
+                </div>
+                <div class="record__point">
+                  <span class="point__value">9,999</span>
+                  <span class="point__unit">kcal</span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="record__item">
+                <div class="record__detail">
+                  <img src="@/assets/icons/icon-vod.svg" class="w-34" />
+                  <div class="line-height-1">
+                    <h3 class="detail__title">22’ 5월 3일 SPINNING LIVE CLASS ㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㅁㄹ</h3>
+                    <span class="detail__text">디파이, 암웨이 강사님 ㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹ</span>
+                  </div>
+                </div>
+                <div class="record__point">
+                  <span class="point__value">9,999</span>
+                  <span class="point__unit">kcal</span>
+                </div>
+              </div>
+            </li>
+          </ul>
         </section>
-        <section class="exercise-share__section" v-if="false">
+        <!-- <section class="exercise-share__section" v-if="false">
           <h2 class="section__title">
             클럽타올라 배지
             <span>15</span>
@@ -124,6 +159,48 @@
           <div v-else class="exercise-share__badge-group">
             <ul class="list-style-none">
               <li v-for="(list, index) in 4" :key="index">
+                <div class="badge__item">
+                  <figure class="badge__figure">
+                    <img src="@/assets/imgs/dummy-musclepin.png" alt="뱃지" />
+                  </figure>
+                  <p class="badge__text">미션 달성 1회</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section> -->
+        <section class="exercise-share__section">
+          <h2 class="section__title">
+            클럽타올라 배지
+            <span>15</span>
+          </h2>
+          <div class="exercise-share__badge-group">
+            <ul class="list-style-none">
+              <li>
+                <div class="badge__item">
+                  <figure class="badge__figure">
+                    <img src="@/assets/imgs/dummy-musclepin.png" alt="뱃지" />
+                  </figure>
+                  <p class="badge__text">미션 달성 1회</p>
+                </div>
+              </li>
+              <li>
+                <div class="badge__item">
+                  <figure class="badge__figure">
+                    <img src="@/assets/imgs/dummy-musclepin.png" alt="뱃지" />
+                  </figure>
+                  <p class="badge__text">미션 달성 1회</p>
+                </div>
+              </li>
+              <li>
+                <div class="badge__item">
+                  <figure class="badge__figure">
+                    <img src="@/assets/imgs/dummy-musclepin.png" alt="뱃지" />
+                  </figure>
+                  <p class="badge__text">미션 달성 1회</p>
+                </div>
+              </li>
+              <li>
                 <div class="badge__item">
                   <figure class="badge__figure">
                     <img src="@/assets/imgs/dummy-musclepin.png" alt="뱃지" />
@@ -201,10 +278,10 @@ export default {
     height: 60px;
     margin-bottom: 20px;
     .header__title {
-      font-size: 18px;
-      font-weight: 800;
-      line-height: 25px;
-      letter-spacing: -.3px;
+      font-size: 16px;
+      font-weight: 700;
+      line-height: 23px;
+      letter-spacing: -.1px;
     }
     .logo {
       position: absolute;
@@ -216,27 +293,31 @@ export default {
   .exercise-share__title {
     display: flex;
     align-items: center;
-    font-size: 18px;
+    font-size: 16px;
     font-weight: 800;
-    line-height: 25px;
+    line-height: 24px;
     img {
-      margin-right: 4px;
+      margin-right: 6px;
+    }
+    span {
+      letter-spacing: -.27px;
     }
   }
   .exercise-share__point {
     display: flex;
     align-items: baseline;
     line-height: 1;
-    margin-bottom: 20px;
+    margin-bottom: 14px;
     .point__value {
-      font-size: 46px;
+      font-size: 40px;
       font-weight: 700;
-      line-height: 56px;
-      letter-spacing: -1px;
+      line-height: 54px;
+      letter-spacing: -.92px;
     }
     .point__unit {
-      font-size: 18px;
-      letter-spacing: -.3px;
+      font-weight: 700;
+      font-size: 16px;
+      letter-spacing: -.1px;
       margin-left: 4px;
     }
   }
@@ -244,10 +325,11 @@ export default {
   .exercise-share__record {
     display: flex;
     margin-bottom: 4px;
+    overflow-x: auto;
     .record__divide {
       display: flex;
       flex-direction: column;
-      flex: 1 1 0;
+      flex: 1 1 auto;
     }
     .record__divide + .record__divide {
       margin-left: 4px;
@@ -260,10 +342,11 @@ export default {
       padding: 20px;
       height: 100%;
       .panel__title {
-        font-size: 16px;
+        font-weight: 800;
+        font-size: 14px;
         line-height: 23px;
-        letter-spacing: -.3px;
-        margin-bottom: 18px;
+        letter-spacing: -.26px;
+        margin-bottom: 16px;
       }
       .panel__point {
         display: flex;
@@ -271,13 +354,13 @@ export default {
         line-height: 1;
         margin-top: auto;
         .point__value {
-          font-size: 36px;
-          letter-spacing: -.3px;
+          font-size: 32px;
+          letter-spacing: -.74px;
           font-weight: 700;
         }
         .point__unit {
           font-size: 18px;
-          letter-spacing: -.3px;
+          letter-spacing: -.36px;
           margin-left: 4px;
           font-weight: 800;
         }
@@ -293,33 +376,57 @@ export default {
     background: $bg-secondary;
     border-radius: 10px;
     li + li {
-      border-top: 1px solid #070D18;
+      border-top: 1px solid #000;
     }
     .record-share__item {
       display: flex;
       align-items: center;
       justify-content: space-between;
       height: 70px;
+      .record-share__right {
+        display: flex;
+        align-items: baseline;
+      }
+      .record-share__text {
+      font-weight: 700;
+      font-size: 14px;
+      line-height: 21px;
+      letter-spacing: -.1px;
+      }
+      .record-share__data {
+        font-weight: 800;
+        font-size: 24px;
+        line-height: 31px;
+        letter-spacing: -.3px;
+        text-align: right;
+        &:not(:last-child) {
+          margin-right: 4px;
+        }
+      }
     }
   }
 
   .exercise-share__section + .exercise-share__section {
-    margin-top: 56px;
+    margin-top: 40px;
   }
 
   .exercise-share__section {
     .section__title {
       display: flex;
       align-items: center;
-      font-size: 18px;
-      font-weight: 800;
-      line-height: 25px;
+      font-size: 16px;
+      font-weight: 700;
+      line-height: 23px;
       letter-spacing: -.3px;
-      margin-bottom: 20px;
+      margin-bottom: 14px;
       span {
         margin-left: 4px;
         color: $gray02;
       }
+    }
+    .text-guide {
+      margin-top: 9px;
+      line-height: 21px;
     }
   }
 
@@ -349,44 +456,53 @@ export default {
       padding: 0 20px;
     }
     .record__detail {
+      flex: 1;
       display: flex;
       align-items: center;
       img {
-        margin-right: 12px;
+        margin-right: 10px;
       }
       .detail__title {
-        font-size: 16px;
+        display: -webkit-box;
+        font-size: 14px;
         font-weight: 800;
-        line-height: 23px;
+        line-height: 1.36;
         letter-spacing: -.3px;
-        width: 180px;
+        // width: 180px;
         overflow: hidden;
         text-overflow: ellipsis;
-        white-space: nowrap;
+        // white-space: nowrap;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
       }
       .detail__text {
-        display: block;
-        font-size: 14px;
-        font-weight: 400;
-        line-height: 21px;
+        display: -webkit-box;
+        font-size: 12px;
+        font-weight: 500;
+        line-height: 18px;
         letter-spacing: -.3px;
         color: $gray02;
-        width: 180px;
+        // width: 180px;
         overflow: hidden;
         text-overflow: ellipsis;
-        white-space: nowrap;
+        // white-space: nowrap;
+        -webkit-line-clamp: 1;
+        -webkit-box-orient: vertical;
       }
     }
     .record__point {
+      flex: none;
       display: flex;
       align-items: baseline;
+      margin-left: 16px;
       line-height: 1;
       .point__value {
-        font-size: 20px;
+        font-weight: 800;
+        font-size: 24px;
         letter-spacing: -.3px;
       }
       .point__unit {
-        font-size: 14px;
+        font-size: 16px;
         letter-spacing: -.1px;
         margin-left: 4px;
       }
